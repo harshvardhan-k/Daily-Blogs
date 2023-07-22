1006b8f09258d695f94db5627bdad739c2b58387
Harshvardhan Kushwaha <harshvardhankushwaha405@gmail.com>
2023-07-22 17:10:46 +0530

    rm

--- api/db/database.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log('Connected to database'))
-.catch(err=>console.log(err));
